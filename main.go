5153d1f99efdb8018dde54ff19a6905604bcf884
Jay Mohile <jamohile@gmail.com>
2023-01-21 04:18:11 -0500

    fix: remove main content

--- main.go
+++ main.go
@@ -1,32 +1,3 @@
 package main
 
-import (
-	"fmt"
-	"time"
-
-	"dash/sweep"
-)
-
-func main() {
-	s := sweep.Sweep[int, int]{
-		Generator: func(c chan int, manager sweep.Manager) {
-			for i := 0; i < 100; i++ {
-				c <- i
-			}
-			close(c)
-		},
-		Worker: func(c int, r chan int, manager sweep.Manager) {
-			if c >= 3 {
-				fmt.Println("cancel")
-				manager.Cancel()
-			}
-			time.Sleep(100 * time.Millisecond)
-			if !manager.IsDone() {
-				r <- c
-			}
-		},
-		MaxWorkers: 1,
-	}
-
-	fmt.Println(s.Run())
-}
+func main() {}
